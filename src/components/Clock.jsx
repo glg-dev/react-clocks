11887cf17d314a634333670d4d3307431115cd4b
Gaël <gael77@hotmail.fr>
2022-05-02 14:57:46 +0200

    debug seconds + refacto scss

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -30,9 +30,9 @@ const Clock = () => {
     setNumericDisplay(date2);
   }
 
-  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
-  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
-  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
+  const secondsDegrees = (seconds / 60) * 360;
 
   useEffect(() => {
     const interval = setInterval(() => {
